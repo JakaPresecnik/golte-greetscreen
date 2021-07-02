814d39fbcafc699527ef5caf5409ce6267e90418
JakaPresecnik <jaka.presecnik@gmail.com>
2021-07-02 20:13:02 +0200

    background added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import Ostalo from './components/Ostalo'
 import './styles/helper.css'
 import logo from './logo.png'
 
+function importAllImages(r) {
+  let images = {};
+  r.keys().map((image, index) => { images[index] = r(image); });
+  return images;
+}
+const images = importAllImages(require.context('./bcgr-img', false, /\.(png|jpe?g|svg)$/));
+console.log(images[0])
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage: `url(${images[0].default})`}}>
       <div id="vsi-podatki"></div>
       <img className="golte-logo" src={logo} />
       <Vreme />
