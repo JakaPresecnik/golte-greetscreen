5980a8f55505f82949cbad276dc01b0295e75d09
JakaPresecnik <jaka.presecnik@gmail.com>
2021-07-04 19:50:10 +0200

    dinamic backgroud and dinamic code for background added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import styled, { css, keyframes } from "styled-components";
 import Vreme from './components/Vreme'
 import Ostalo from './components/Ostalo'
 import './styles/helper.css'
@@ -11,12 +12,31 @@ function importAllImages(r) {
 const images = importAllImages(require.context('./bcgr-img', false, /\.(png|jpe?g|svg)$/));
 
 function App() {
+  const slide = keyframes`
+        0% {left: 100%}
+        10% {left: 0%}
+        ${100 / Object.keys(images).length}% {left: 0%}
+        ${100 / Object.keys(images).length + 10}% {left: -100%}
+        100% {left: -100%}
+    `
+    const autoplay3 = props => {
+
+    return css`
+        ${Object.keys(images).length * 10}s ${slide} infinite ease-in-out;
+    `}
+    const Img = styled.img`
+        position: absolute;
+        left: -100%;
+        width: 100vw;
+        animation: ${autoplay3};
+    `
   return (
     <div className="App">
       <div id="vsi-podatki"></div>
       <div className="background">
-        <img src={images[1].default} />
-        <img src={images[1].default} />
+        {Object.keys(images).map((img, i) => (
+          <Img style={{animationDelay: 10 * i + 's'}} src={images[img].default} />
+        ))}
       </div>
       <img className="golte-logo" src={logo} />
       <Vreme />
